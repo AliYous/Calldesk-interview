4f7ffdbdf8b7997277516908bfa8e0fb6ecb6cbc
Ali Youssef <aliyous@Alis-MacBook-Pro.local>
2020-11-23 20:03:35 +0100

    created initial folders and component files

---
+++ src/components/CallsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CallsList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CallsList
---
+++ src/components/CallsListItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CallsListItem() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CallsListItem
