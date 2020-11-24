5262da2437919a116f83ef2e17626a7af6bb9a89
Ali Youssef <aliyous@Alis-MacBook-Pro.local>
2020-11-24 11:29:00 +0100

    created CallsList and CallsListItem componenent to display the fetched list of calls

--- src/components/CallsList.js
+++ src/components/CallsList.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { Container, Divider } from '@material-ui/core';
+import React, { useContext } from 'react';
+import {CallsContext} from '../context/CallsContext';
+import './callsList.css';
+import CallsListItem from './CallsListItem';
+
 
 function CallsList() {
+  const { calls , callsCounter } = useContext(CallsContext);
+  
+
   return (
-    <div>
-      
-    </div>
+    <Container maxWidth="xs" className="callsList">
+      <div className="callsList_callsCounterDiv">
+        <h5>{callsCounter} calls</h5>
+      </div>
+      <Divider />
+
+      <div className="callsList_scrollableDiv margin_top1">
+       { calls && calls.map((call, index) =>
+          <CallsListItem key={call.sessionId} call={call} />
+        )} 
+
+
+      </div>
+    </Container>
   )
 }
+
+
 
 export default CallsList
--- src/components/CallsListItem.js
+++ src/components/CallsListItem.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Card, CardContent, Typography, Divider } from '@material-ui/core';
 
-function CallsListItem() {
+import './callsListItem.css'
+
+function CallsListItem(props) {
+  const callStartTime = new Date(props.call.callStartTime).toISOString().substr(11,5);
+  const callDate = new Date(props.call.callStartTime).toLocaleDateString().substring(0,10);
+  const callDuration = new Date(props.call.callDuration).toISOString().slice(11,19);
+
+  const handleOnClick = (index) => {
+    console.log(index)
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Card className="classListItem" variant="outlined" onClick={() => handleOnClick(index)}>
+        <CardContent className="classListItem_card">
+          <div className="classListItem_callTimeAndDateDiv">
+            <Typography variant="h5" component="h2">
+              { callStartTime }
+            </Typography>
+            <Typography className="classListItem_callDate" variant="body2" component="p" color="textSecondary">
+              { callDate }
+            </Typography>
+          </div>
+          <Typography className="classListItem_callDuration" color="textSecondary">
+              { callDuration }
+          </Typography>
+        </CardContent>
+      </Card>
+      <Divider />
+    </>
   )
 }
 
