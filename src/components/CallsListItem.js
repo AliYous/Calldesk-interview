1c5ab07f94cfae613400c2c0a223db9113588b45
Ali Youssef <aliyous@Alis-MacBook-Pro.local>
2020-11-25 22:17:22 +0100

    fixed callDuration variable in CallsListItem, it was always showing one second more than the real duration

--- src/components/CallsListItem.js
+++ src/components/CallsListItem.js
@@ -7,7 +7,7 @@ import './callsListItem.css'
 function CallsListItem({call, noHover}) {
   const callStartTime = new Date(call.callStartTime).toISOString().substr(11,5);
   const callDate = new Date(call.callStartTime).toLocaleDateString().substring(0,10);
-  const callDuration = new Date(call.callDuration*1000).toISOString().slice(11,19);
+  const callDuration = new Date(call.callDuration*1000-1).toISOString().slice(11,19);
 
   const {setSelectedCallState} = useContext(CallsContext);
 
